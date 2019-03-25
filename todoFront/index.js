8a78cc896ece2c244dcdb737779a1ec831bba895
TyrionJYQ <1003518719@qq.com>
2019-03-25 16:45:35 +0800

    start todoFront import antd

--- todoFront/index.js
+++ todoFront/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Login from './src/components/login';
+// antd
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import "./src/common/css/common.css"
+import "antd/dist/antd.css";
 
-class HelloMessage extends React.Component {
+moment.locale('zh-cn');
+
+
+class Page extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return <Login />
   }
 }
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+ReactDOM.render(<Page />, mountNode);
