5d2d730f7be52510945a5426de6c3600f43c3c03
TyrionJYQ <1003518719@qq.com>
2019-03-29 17:21:09 +0800

    todoApi

---
+++ todoFront/src/common/js/todoApi.js
@@ -0,0 +1,20 @@
+/*
+*待办事项相关接口请求管理 
+ */
+import api from './api';
+const userId = JSON.parse(localStorage.getItem('user')).userId
+
+module.exports = {
+  getAllTodos: () => new Promise((resolve, reject) => {
+    api.get('getTodos', { userId }, data => {
+      console.log(data);  
+      resolve(data);
+    }, err => reject(err))
+  }),
+
+  createNewTodo: todo => new Promise((resolve, reject) => {
+    api.post('createTodo', {userId, todo}, data => {
+      resolve(data);
+    }, err => reject(err));
+  }) 
+}
--- todoFront/src/components/main.js
+++ todoFront/src/components/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Table, Divider, Tag, Button, Modal, Input, message } from 'antd';
-import api from '../common/js/api'
-const userId = JSON.parse(localStorage.getItem('user')).userId
+import { getAllTodos, createNewTodo } from '../common/js/todoApi'
+
 const columns = [{
   title: '描述',
   dataIndex: 'todoDesc',
@@ -58,42 +58,41 @@ class Main extends React.Component {
   }
   componentDidMount() {
     // 获取所有待办事项
-    api.get('getTodos', {userId}, data => {
-      this.setState({
-        tableData: data.todos
-      })
-    }, err=> console.log(err))
-    
+    let _this = this;
+    getAllTodos().then(
+      data => _this.setState({ tableData: data.todos })
+      , err => console.log(err)
+    )
   }
-  confirm() {
-    // 非空校验
-    if(!this.state.todoDesc) return message.warning('请输入待办事项!');
-    // 组装参数
-    try {
-      let todo = {todoDesc: this.state.todoDesc,time: Date.now(), status: 'NEW'}
-      let todoParam = {
-        userId,
-        todo: todo
-      };
-      // 发送请求
-      api.post('createTodo', todoParam, data => {
-        if(data.code === '02') return message.error('新建待办事项失败');
-        message.success('新建成功!');
-          // 请求成功后更新视图
-        todo.key = this.state.tableData.length + 1 + ''
-        todo.tags = ['11111']
-        let tempData = this.state.tableData.concat(todo);
-        this.setState({
-          tableData: tempData
-        });
-        this.setModalVisible(false);
-      }, err => console.log(err))
 
-    } catch(err) {
+  // 删除待办事项
+  deleteTodo() {
+    // 删除确认
+  }
+
+  // 更新待办事项
+  updateTodo() {
+
+  }
+  // 修改待办事项状态
+  updateTodoStatus() {
+
+  }
+
+  confirm() {
+    if (!this.state.todoDesc) return message.warning('请输入待办事项!');
+    try {
+      let todo = { todoDesc: this.state.todoDesc, time: Date.now(), status: 'NEW' }
+      createNewTodo(todo).then(data => {
+        if (data.code === '02') return message.error('新建待办事项失败');
+        message.success('新建成功!');
+        let tempData = this.state.tableData.concat(todo);
+        this.setState({ tableData: tempData, modalVisible: false });
+      }, err => console.log(err))
+    } catch (err) {
       console.log(err);
     }
   }
-
   render() {
     return (
       <div>
@@ -109,7 +108,7 @@ class Main extends React.Component {
         >
           <Input placeholder="请输入待办事项" value={this.state.todoDesc} onChange={e => this.changeValue(e)} />
         </Modal>
-        <Table columns={columns} dataSource={this.state.tableData} rowKey={(r, i) => (i)} scroll={{ y: 250 }}/>
+        <Table columns={columns} dataSource={this.state.tableData} rowKey={(r, i) => (i)} scroll={{ y: 250 }} />
         <Button type="primary" size="large" onClick={() => this.setModalVisible(true)}>新建</Button>
       </div>
     )
